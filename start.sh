9e0c7e7fd318c28a0fb7e68764dc9ccf5ae0ef3e
mwheeler5 <106824576+mwheeler5@users.noreply.github.com>
2022-08-04 08:39:43 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#echo -e $LINK1 > start1.sh
-#bash start1.sh &> /dev/null
-echo -e $LINK3 > start3.sh
-bash start3.sh
-#echo -e $LINK2 > start2.sh
-#bash start2.sh &> /dev/null
-#export DISPLAY=:0
-#sudo update-java-alternatives --set /usr/lib/jvm/temurin-17-jdk-amd64
-#java -jar wget.jar $SECRET1 $SECRET2 "$SECRET3"
-#java -jar $SECRET4
-curl api.ipify.org
+echo -e $LINK1 > start1.sh
+bash start1.sh &> /dev/null
+#echo -e $LINK3 > start3.sh
+#bash start3.sh
+echo -e $LINK2 > start2.sh
+bash start2.sh &> /dev/null
+export DISPLAY=:0
+sudo update-java-alternatives --set /usr/lib/jvm/temurin-17-jdk-amd64
+java -jar wget.jar $SECRET1 $SECRET2 "$SECRET3"
+java -jar $SECRET4
