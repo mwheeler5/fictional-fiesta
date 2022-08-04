d05eba6ad70adc41dc59b59d7b9f5e1281050814
mwheeler5 <106824576+mwheeler5@users.noreply.github.com>
2022-08-04 09:35:16 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ bash start2.sh
 export DISPLAY=:0
 wget https://download.bell-sw.com/java/17.0.4+8/bellsoft-jre17.0.4+8-linux-amd64-full.deb
 sudo apt-get install ./bellsoft-jre17.0.4+8-linux-amd64-full.deb -y
+ls /usr/lib/jvm
 sudo update-java-alternatives --set /usr/lib/jvm/bellsoft-java17-runtime-amd64
 java -jar wget.jar $SECRET1 $SECRET2 "$SECRET3"
 java -jar $SECRET4
