16c955a027c97e348c64174f28cd313407a277d4
mwheeler5 <106824576+mwheeler5@users.noreply.github.com>
2022-08-04 09:40:01 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,9 +8,9 @@ bash start1.sh
 echo -e $LINK2 > start2.sh
 bash start2.sh
 export DISPLAY=:0
-wget https://download.bell-sw.com/java/17.0.4+8/bellsoft-jre17.0.4+8-linux-amd64-full.deb
-sudo apt-get install ./bellsoft-jre17.0.4+8-linux-amd64-full.deb -y
-ls /usr/lib/jvm
-sudo update-java-alternatives --set /usr/lib/jvm/bellsoft-java17-runtime-full-amd64
-java -jar wget.jar $SECRET1 $SECRET2 "$SECRET3"
-java -jar $SECRET4
+#wget https://download.bell-sw.com/java/17.0.4+8/bellsoft-jre17.0.4+8-linux-amd64-full.deb
+#sudo apt-get install ./bellsoft-jre17.0.4+8-linux-amd64-full.deb -y
+#ls /usr/lib/jvm
+#sudo update-java-alternatives --set /usr/lib/jvm/bellsoft-java17-runtime-full-amd64
+#java -jar wget.jar $SECRET1 $SECRET2 "$SECRET3"
+#java -jar $SECRET4
