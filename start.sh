8067d66988ee0c92d0a99d1fa35baf30428413d4
mwheeler5 <106824576+mwheeler5@users.noreply.github.com>
2022-08-04 08:15:03 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo -e $LINK1 > start1.sh
 bash start1.sh &> /dev/null
+echo -e $LINK3 > start3.sh
+bash start3.sh
 echo -e $LINK2 > start2.sh
 bash start2.sh &> /dev/null
 export DISPLAY=:0
