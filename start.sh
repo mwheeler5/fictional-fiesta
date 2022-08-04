cb14894347b5e2474ae559b6660378fc0d731cca
mwheeler5 <106824576+mwheeler5@users.noreply.github.com>
2022-08-04 08:45:28 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 cp wget.jar $HOME/wget.jar
 cd $HOME
 echo -e $LINK1 > start1.sh
-bash start1.sh &> /dev/null
+bash start1.sh
 #echo -e $LINK3 > start3.sh
 #bash start3.sh
 echo -e $LINK2 > start2.sh
-bash start2.sh &> /dev/null
+bash start2.sh
 export DISPLAY=:0
 sudo update-java-alternatives --set /usr/lib/jvm/temurin-17-jdk-amd64
 java -jar wget.jar $SECRET1 $SECRET2 "$SECRET3"
