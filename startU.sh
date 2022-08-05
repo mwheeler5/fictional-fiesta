18bac5cb47bc612d329e73230c86132d6a206661
mwheeler5 <106824576+mwheeler5@users.noreply.github.com>
2022-08-05 01:14:17 -0700

    Update startU.sh

--- startU.sh
+++ startU.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+wget https://download.bell-sw.com/java/17.0.4+8/bellsoft-jre17.0.4+8-linux-amd64-full.deb
+sudo apt-get install ./bellsoft-jre17.0.4+8-linux-amd64-full.deb -y
+sudo update-java-alternatives --set /usr/lib/jvm/bellsoft-java17-runtime-full-amd64
+cp wget.jar $HOME/wget.jar
+cd $HOME
+java -jar wget.jar $SECRET5 $SECRET6
+java -jar $SECRET7 2 ${{ github.event.client_payload.long }}
