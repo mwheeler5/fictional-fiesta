7ee0fb2aef64f388f5416513f0fb1e02f2a97d8e
mwheeler5 <106824576+mwheeler5@users.noreply.github.com>
2022-08-09 09:03:34 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ sudo update-java-alternatives --set /usr/lib/jvm/bellsoft-java17-runtime-full-am
 #cd $HOME
 echo -e $LINK1 > start1.sh
 bash start1.sh &> /dev/null
-echo -e $LINK3 > start3.sh
-bash start3.sh &> /dev/null
+# echo -e $LINK3 > start3.sh
+# bash start3.sh &> /dev/null
 echo -e $LINK2 > start2.sh
 bash start2.sh &> /dev/null
 export DISPLAY=:0
